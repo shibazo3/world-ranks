f0724dd39e8431da6437dd7a4a282d01573d2116
t-shibata <shibazou3@gmail.com>
2021-02-04 18:59:44 +0900

    change getServerSide to getStatic

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -109,11 +109,20 @@ const Country = ({ country }) => {
 
 export default Country;
 
-export const getServerSideProps = async ({ params }) => {
-  const res = await fetch(
-    `https://restcountries.eu/rest/v2/alpha/${params.id}`
-  );
+export const getStaticPaths = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
 
+  const paths = countries.map((country) => ({
+    params: { id: country.alpha3Code },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
   const country = await getCountry(params.id);
   return {
     props: { country },
